2a051342fb24a7b7a13e1e07b1df8643fb502f48
Frands Otting <frands@otting.dk>
2021-02-12 21:32:03 +0100

    remove alacritty for now

--- hyper-apps.lua
+++ hyper-apps.lua
@@ -3,10 +3,10 @@
 -- To launch _your_ most commonly-used apps via Hyper Mode, create a copy of
 -- this file, save it as `hyper-apps.lua`, and edit the table below to configure
 -- your preferred shortcuts.
-return {
+local hyper_apps = {
   {"b", "Google Chrome"}, -- "B" for "Browser"
   {"f", "Finder"}, -- "F" for "Finder"
   {"g", "Mailplane"}, -- "G" for "Gmail"
-  {"i", "iTerm"},
-  {"t", "Alacritty"} -- "T" for "Terminal"
+  {"i", "iTerm"} -- "i" for iTerm
 }
+return hyper_apps
